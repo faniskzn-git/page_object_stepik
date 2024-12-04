141ef5d7cc93f8ec38ef95f30f10fae1686fc2a8
faniskzn <faniskzn@gmail.com>
2024-12-04 12:08:03 +0300

    добавлен тест для проверки добавления товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
+    BASKET_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main > p.price_color")
+    BASKET_MESSAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+import time
+
+
+class ProductPage(BasePage):
+    def should_be_product_add_to_basket(self):
+        self.should_be_see_add_to_basket_button()
+        self.go_to_basket_page()
+        self.solve_quiz_and_get_code()
+        self.should_be_product_name_equal_basket_name()
+        self.should_be_basket_amount_equal_product_price()
+
+    def should_be_see_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Add to basket button is not presented"
+
+    def go_to_basket_page(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        basket_button.click()
+
+    def should_be_product_name_equal_basket_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        basket_message_product_name = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PRODUCT_NAME).text
+        assert product_name == basket_message_product_name, "Product name does not match message in basket"
+
+    def should_be_basket_amount_equal_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text.split(' ')[0]
+        basket_message_product_price = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PRODUCT_PRICE).text.split(' ')[0]
+        assert product_price == basket_message_product_price, "Product price does not match message in basket"
